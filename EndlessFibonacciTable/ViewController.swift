76ed0be6ebed09d1b055f943cf08b122af876746
Adam <adam.tecle@gmail.com>
2016-12-08 18:46:32 -0500

    A sketched solution in ViewController.swift

--- EndlessFibonacciTable/ViewController.swift
+++ EndlessFibonacciTable/ViewController.swift
@@ -10,18 +10,104 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate var data: [Int] = [0, 1]
+    fileprivate var indexOfLastFibNumberCalculated = 1
+    fileprivate var isRetrievingNumbers = false
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Set up
+    
+    private func setup() {
+        configureTableView()
+        loadData()
     }
 
+    private func configureTableView() {
+        tableView.register(LoadingCell.self, forCellReuseIdentifier: Constants.LoadingCellIdentifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.LoadingCellIdentifier)
+    }
+    
+    fileprivate func loadData() {
+        
+        retrieveNextFibonacciNumbers { [unowned self] (sequence) in
+            self.data.append(contentsOf: sequence)
+            self.tableView.reloadData()
+            self.isRetrievingNumbers = false
+        }
+        
+        isRetrievingNumbers = true
+    }
+    
+    // MARK: - Data
+    
+    private func retrieveNextFibonacciNumbers(_ completion: ((_ seq: [Int]) -> ())) {
+        
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0 {
+            return
+        }
+        
+        guard let loadingCell = cell as? LoadingCell else {
+            return
+        }
+        
+        loadingCell.startAnimating()
+        
+        if isRetrievingNumbers == false {
+            loadData()
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 1 : 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.DefaultCellIdentifier) else {
+                let cell = UITableViewCell()
+                //configure cell
+                return cell
+            }
+            
+            //configure cell
+            return cell
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.LoadingCellIdentifier) as? LoadingCell
+            
+            guard let loadingCell = cell else {
+                return LoadingCell()
+            }
+            
+            return loadingCell
+        }
+        
+    }
 }
 
