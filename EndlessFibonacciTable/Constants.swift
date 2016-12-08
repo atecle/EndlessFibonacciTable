06dbf8b73512fcf71a7a517c79898568e7b94f04
Adam <adam.tecle@gmail.com>
2016-12-08 18:14:14 -0500

    Adding Constants file

---
+++ EndlessFibonacciTable/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  EndlessFibonacciTable
+//
+//  Created by Adam on 12/8/16.
+//  Copyright © 2016 Adam Tecle. All rights reserved.
+//
+
+struct Constants {
+
+    static let LoadingCellIdentifier = "LoadingCell"
+}
