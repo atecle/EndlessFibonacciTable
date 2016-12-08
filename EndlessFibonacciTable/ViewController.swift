a62953e5922558bcd8f7781871599390d6350c8f
Adam <adam.tecle@gmail.com>
2016-12-08 18:04:59 -0500

    Initial commit 🎉

---
+++ EndlessFibonacciTable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EndlessFibonacciTable
+//
+//  Created by Adam on 12/8/16.
+//  Copyright © 2016 Adam Tecle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
